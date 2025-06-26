bab169c16f37828e2961889a492b38b4f96c98ef
lanath <valentin@viviersoft.com>
2025-06-26 10:24:17 +0200

    feat: added compose and pipe functions
    Added Compose4, Compose5, Pipe4, and Pipe5 functions to support function composition with up to five arguments.
    Added ComposeN and PipeN functions to support function composition with an arbitrary number of functions that takes and return the same arguments.
    Added ComposeUnsafe and PipeUnsafe functions to support function composition with an arbitrary number of functions taking any as arguments and return values.

--- func.go
+++ func.go
@@ -41,21 +41,76 @@ func Partial5[T1, T2, T3, T4, T5, T6, R any](f func(T1, T2, T3, T4, T5, T6) R, a
 }
 
 // Compose returns new function that, when called, returns the result of calling g and then f with the result from g
-func Compose[T, U, V any](f func(U) V, g func(T) U) func (T) V {
+func Compose[T, U, V any](f func(U) V, g func(T) U) func(T) V {
 	return func(t T) V {
 		return f(g(t))
 	}
 }
 
-func Compose3[T1, T2, T3, R any](f func(T3) R, g func(T2) T3, h func(T1) T2) func (T1) R {
+func Compose3[T1, T2, T3, R any](f func(T3) R, g func(T2) T3, h func(T1) T2) func(T1) R {
 	return Compose(f, Compose(g, h))
 }
 
+func Compose4[T1, T2, T3, T4, R any](f func(T4) R, g func(T3) T4, h func(T2) T3, i func(T1) T2) func(T1) R {
+	return Compose(f, Compose(g, Compose(h, i)))
+}
+
+func Compose5[T1, T2, T3, T4, T5, R any](f func(T5) R, g func(T4) T5, h func(T3) T4, i func(T2) T3, j func(T1) T2) func(T1) R {
+	return Compose(f, Compose(g, Compose(h, Compose(i, j))))
+}
+
+func ComposeN[T any](funcs ...func(T) T) func(T) T {
+	return func(value T) T {
+		for i := len(funcs) - 1; i >= 0; i-- {
+			value = funcs[i](value)
+		}
+		return value
+	}
+}
+
+func ComposeUnsafe(funcs ...AnyFunc) func(interface{}) interface{} {
+	return func(value interface{}) interface{} {
+		for i := len(funcs) - 1; i >= 0; i-- {
+			value = funcs[i](value)
+		}
+		return value
+	}
+}
+
 // Pipe returns new function that, when called, returns the result of calling f and then g with the result from f
-func Pipe[T, U, V any](f func(T) U, g func(U) V) func (T) V {
+func Pipe[T, U, V any](f func(T) U, g func(U) V) func(T) V {
 	return Compose(g, f)
 }
 
-func Pipe3[T1, T2, T3, R any](f func(T1) T2, g func(T2) T3, h func(T3) R) func (T1) R {
+func Pipe3[T1, T2, T3, R any](f func(T1) T2, g func(T2) T3, h func(T3) R) func(T1) R {
 	return Pipe(f, Pipe(g, h))
+}
+
+func Pipe4[T1, T2, T3, T4, R any](f func(T1) T2, g func(T2) T3, h func(T3) T4, i func(T4) R) func(T1) R {
+	return Pipe(f, Pipe(g, Pipe(h, i)))
+}
+
+func Pipe5[T1, T2, T3, T4, T5, R any](f func(T1) T2, g func(T2) T3, h func(T3) T4, i func(T4) T5, j func(T5) R) func(T1) R {
+	return Pipe(f, Pipe(g, Pipe(h, Pipe(i, j))))
+}
+
+func PipeN[T any](funcs ...func(T) T) func(T) T {
+	return func(value T) T {
+		for _, fn := range funcs {
+			value = fn(value)
+		}
+		return value
+	}
+}
+
+type AnyFunc func(interface{}) interface{}
+
+// PipeUnsafe is a version of Pipe that accepts AnyFunc, allowing for more flexible function signatures.
+func PipeUnsafe(funcs ...AnyFunc) func(interface{}) interface{} {
+	return func(value interface{}) interface{} {
+		for _, fn := range funcs {
+			value = fn(value)
+		}
+		return value
+	}
 }
--- func_test.go
+++ func_test.go
@@ -107,6 +107,80 @@ func TestCompose3(t *testing.T) {
 	is.Equal(15, mulBy3SumBy2AndMulBy3(val))
 }
 
+func TestCompose4(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	// Compose4: (((sumBy2 -> mulBy3) -> sumBy2) -> mulBy3)
+	composed := Compose4(mulBy3, sumBy2, mulBy3, sumBy2)
+	val := 1
+	// (((1+2)=3) *3=9) +2=11, *3=33
+	is.Equal(33, composed(val))
+
+	// Compose4: (((mulBy3 -> sumBy2) -> mulBy3) -> sumBy2)
+	composed2 := Compose4(sumBy2, mulBy3, sumBy2, mulBy3)
+	// (((1*3)=3)+2=5)*3=15, +2=17
+	is.Equal(17, composed2(val))
+}
+
+func TestCompose5(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	composed := Compose5(sumBy2, mulBy3, sumBy2, mulBy3, sumBy2)
+	val := 1
+	// ((((1+2)=3)*3=9)+2=11)*3=33, +2=35
+	is.Equal(35, composed(val))
+
+	composed2 := Compose5(mulBy3, sumBy2, mulBy3, sumBy2, mulBy3)
+	// ((((1*3)=3)+2=5)*3=15)+2=17, *3=51
+	is.Equal(51, composed2(val))
+}
+
+func TestComposeN(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	val := 1
+	// ComposeN applies right-to-left
+	composed := ComposeN(sumBy2, mulBy3, sumBy2, mulBy3, sumBy2)
+	// (((((1+2)=3)*3=9)+2=11)*3=33)+2=35
+	is.Equal(35, composed(val))
+
+	composed2 := ComposeN(mulBy3, sumBy2, mulBy3, sumBy2, mulBy3)
+	// (((((1*3)=3)+2=5)*3=15)+2=17)*3=51
+	is.Equal(51, composed2(val))
+
+	// ComposeN with no functions should return input
+	identity := ComposeN[int]()
+	is.Equal(42, identity(42))
+}
+
+func TestComposeUnsafe(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	add2 := func(x interface{}) interface{} {
+		return x.(int) + 2
+	}
+	mul3 := func(x interface{}) interface{} {
+		return x.(int) * 3
+	}
+	toString := func(x interface{}) interface{} {
+		return strconv.Itoa(x.(int))
+	}
+
+	compose := ComposeUnsafe(toString, mul3, add2)
+	result := compose(4)
+	is.Equal("18", result) // (4 + 2) * 3 = 18, then to string
+
+	compose2 := ComposeUnsafe(toString)
+	is.Equal("7", compose2(7))
+
+	compose3 := ComposeUnsafe()
+	is.Equal(5, compose3(5))
+}
+
 func TestPipe(t *testing.T) {
 	t.Parallel()
 	is := assert.New(t)
@@ -129,4 +203,70 @@ func TestPipe3(t *testing.T) {
 	val := 1
 	is.Equal(11, sumBy2MulBy3AndSumBy2(val))
 	is.Equal(15, mulBy3SumBy2AndMulBy3(val))
+}
+
+func TestPipe4(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	sumBy2MulBy3AndSumBy2 := Pipe4(sumBy2, mulBy3, sumBy2, mulBy3)
+	mulBy3SumBy2AndMulBy3 := Pipe4(mulBy3, sumBy2, mulBy3, sumBy2)
+
+	val := 1
+	is.Equal(33, sumBy2MulBy3AndSumBy2(val))
+	is.Equal(17, mulBy3SumBy2AndMulBy3(val))
+}
+
+func TestPipe5(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	sumBy2MulBy3AndSumBy2 := Pipe5(sumBy2, mulBy3, sumBy2, mulBy3, sumBy2)
+	mulBy3SumBy2AndMulBy3 := Pipe5(mulBy3, sumBy2, mulBy3, sumBy2, mulBy3)
+
+	val := 1
+	is.Equal(35, sumBy2MulBy3AndSumBy2(val))
+	is.Equal(51, mulBy3SumBy2AndMulBy3(val))
+}
+
+func TestPipeN(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	sumBy2MulBy3AndSumBy2 := PipeN(sumBy2, mulBy3, sumBy2, mulBy3, sumBy2)
+	mulBy3SumBy2AndMulBy3 := PipeN(mulBy3, sumBy2, mulBy3, sumBy2, mulBy3)
+
+	val := 1
+	is.Equal(35, sumBy2MulBy3AndSumBy2(val))
+	is.Equal(51, mulBy3SumBy2AndMulBy3(val))
+}
+
+func TestPipeUnsafe(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	// Define AnyFunc functions
+	add2 := func(x interface{}) interface{} {
+		return x.(int) + 2
+	}
+	mul3 := func(x interface{}) interface{} {
+		return x.(int) * 3
+	}
+	toString := func(x interface{}) interface{} {
+		return strconv.Itoa(x.(int))
+	}
+
+	// Compose them using PipeUnsafe
+	pipe := PipeUnsafe(add2, mul3, toString)
+
+	result := pipe(4)
+	is.Equal("18", result) // (4 + 2) * 3 = 18, then to string
+
+	// Test with only one function
+	pipe2 := PipeUnsafe(toString)
+	is.Equal("7", pipe2(7))
+
+	// Test with no functions (should return input)
+	pipe3 := PipeUnsafe()
+	is.Equal(5, pipe3(5))
 }
